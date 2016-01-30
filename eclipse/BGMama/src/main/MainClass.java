a26e88313d17300238b3d735eef5d73b54510ff6
Siyana <siyanaslavova@gmail.com>
2016-01-31 00:25:38 +0200

    add XML parser

--- eclipse/BGMama/src/main/MainClass.java
+++ eclipse/BGMama/src/main/MainClass.java
@@ -1,10 +1,14 @@
 package main;
 
+import model.XMLParser;
+import model.XMLParser.DataType;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//
+		String filePath = new String("src/Supporting Files/TrainingData.txt");
+		XMLParser.parseFile(filePath, DataType.DataTypeTraining);
+		XMLParser.printData(XMLParser.getTrainingData());
 	}
 
 }
