3b4bf75ad6d2affd762ffd5ea32dbedb819217f9
Siyana <siyanaslavova@gmail.com>
2016-02-13 02:11:34 +0200

    update UI

--- eclipse/BGMama/src/view/LocationsTableView.java
+++ eclipse/BGMama/src/view/LocationsTableView.java
@@ -1,13 +1,19 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -15,25 +21,37 @@ import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import org.eclipse.wb.internal.draw2d.Label;
+
+import gate.AnnotationSet;
+import gate.util.FeatureBearer;
 import model.BayesAlgorithm;
 import model.Comment;
 import model.Comment.ClassType;
 import model.DataManager;
+import model.LoadGApp;
 
 public class LocationsTableView extends JFrame {
 
-	/**
-	 * 
-	 */
+	private static final String mapsBaseURL = "https://www.google.bg/maps/search/";
+	private ArrayList<String> hotelsUrls = null;
+	private ArrayList<Comment> commentsDataSource = null;
+	private TableViewType type;
+
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTable table;
 
-	/**
-	 * Create the frame.
-	 */
-	public LocationsTableView() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public enum TableViewType {
+		TableViewTypeCategorizeComments,
+		TableViewTypeFindHotels
+	}
+	
+
+
+	public LocationsTableView(final TableViewType type) {
+		this.type = type;
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -51,25 +69,36 @@ public class LocationsTableView extends JFrame {
 			};     
 
 		};
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		table.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
-//				JTable target = (JTable)e.getSource();
-//				int row = target.getSelectedRow();
-//				Comment comment = DataManager.getTrainingData().get(row);
-//				openFullTextDialog(comment);			
+				JTable target = (JTable)e.getSource();
+				int row = target.getSelectedRow();
+				if(type == TableViewType.TableViewTypeCategorizeComments) {
+					Comment comment = commentsDataSource.get(row);
+					openFullTextDialog(comment.getCommentText());
+				} else if (type == TableViewType.TableViewTypeFindHotels) {
+					openFullTextDialog(hotelsUrls.get(row));
+//					openUrl(hotelsUrls.get(row));
+				}
+							
 			}
 		});
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.setModel(dataModel());
+		if(type == TableViewType.TableViewTypeCategorizeComments) {
+			table.setModel(categorizeCommentsDataModel());
+		} else if (type == TableViewType.TableViewTypeFindHotels) {
+			table.setModel(findHotelsDataModel());
+		}
 		table.setFont(new Font("Calibri", Font.PLAIN, 11));
 
 		JScrollPane scrollPanel = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		contentPane.add(scrollPanel, BorderLayout.CENTER);
 	}
 
-	private void openFullTextDialog(Comment comment) {
+	private void openFullTextDialog(String text) {
 		try {
-			FullTextDialog dialog = new FullTextDialog(comment.getCommentText());
+			FullTextDialog dialog = new FullTextDialog(text);
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -77,21 +106,33 @@ public class LocationsTableView extends JFrame {
 		}
 	}
 
-	private DefaultTableModel dataModel(){
+	private DefaultTableModel categorizeCommentsDataModel(){
 		DefaultTableModel dtm = new DefaultTableModel(0, 0);
 
 		// add header of the table
-		String header[] = new String[] { " ÓÏÂÌÚ‡", " ‡ÚÂ„ÓËˇ"};
+		String header[] = new String[] { "–ö–æ–º–µ–Ω—Ç–∞—Ä", "–ö–∞—Ç–µ–≥–æ—Ä–∏—è"};
 
 		// add header in table model     
 		dtm.setColumnIdentifiers(header);
-		addRowsData(dtm);
+		addCommentsRowsData(dtm);
 		return dtm;
 	}
 
-	private void addRowsData(DefaultTableModel dataModel){
-		ArrayList<Comment> allComments = DataManager.getUnknownData();
-		for (Comment comment : allComments) {
+	private DefaultTableModel findHotelsDataModel(){
+		DefaultTableModel dtm = new DefaultTableModel(0, 0);
+
+		// add header of the table
+		String header[] = new String[] { "–•–æ—Ç–µ–ª", "–õ–∏–Ω–∫"};
+
+		// add header in table model     
+		dtm.setColumnIdentifiers(header);
+		addHotelsRowsData(dtm);
+		return dtm;
+	}
+
+	private void addCommentsRowsData(DefaultTableModel dataModel){
+		commentsDataSource = DataManager.getUnknownData();
+		for (Comment comment : commentsDataSource) {
 			ClassType classifiedType = BayesAlgorithm.classifyComment(comment);
 			System.out.println("Comment type: " + classifiedType);
 			comment.setClassifiedType(classifiedType);
@@ -99,10 +140,10 @@ public class LocationsTableView extends JFrame {
 			String text = comment.getCommentText();
 			switch (type) {
 			case ClassTypePositive:
-				text = "+" + "   " + text;
+				text = "+" + " " + text;
 				break;
 			case ClassTypeNegative:
-				text = "-" + "   " + text;
+				text =  "-" + " " + text;
 				break;
 
 			default:
@@ -112,5 +153,33 @@ public class LocationsTableView extends JFrame {
 			Object [] rowObject = new Object[] { text, type};
 			dataModel.addRow(rowObject);
 		}
+	}
+
+	private void addHotelsRowsData(DefaultTableModel dataModel){
+		AnnotationSet hotelsAnnotationSet = LoadGApp.executeGApp();
+		hotelsUrls = new ArrayList<String>();
+		Iterator hotelsAnnIt = hotelsAnnotationSet.get("Hotel_Name").iterator(); 
+		while (hotelsAnnIt.hasNext()) {
+			String name = ((FeatureBearer) hotelsAnnIt.next()).getFeatures().get("matchStr").toString();
+			String url = mapsBaseURL + name;
+			hotelsUrls.add(url);
+			Object [] rowObject = new Object[] { name,  url };
+			dataModel.addRow(rowObject);
+		}
+	}
+
+	private static void openUrl(String url) {
+		if (Desktop.isDesktopSupported()) {	     
+			try {
+				Desktop.getDesktop().browse(new URI(url));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else { /* TODO: error handling */ }
 	}
 }
--- eclipse/BGMama/src/view/WelcomeScreen.java
+++ eclipse/BGMama/src/view/WelcomeScreen.java
@@ -20,6 +20,9 @@ import javax.swing.border.EmptyBorder;
 import model.BayesAlgorithm;
 import model.Comment;
 import model.DataManager;
+import view.LocationsTableView.TableViewType;
+
+import java.awt.FlowLayout;
 
 public class WelcomeScreen extends JFrame {
 
@@ -49,52 +52,55 @@ public class WelcomeScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public WelcomeScreen() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 184);
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setBounds(100, 100, 450, 197);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(245, 245, 245));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{59, 315, 0};
-		gbl_contentPane.rowHeights = new int[]{61, 23, 0};
-		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		contentPane.setLayout(gbl_contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JLabel welcomeLabel = new JLabel("\u0414\u043E\u0431\u0440\u0435 \u0434\u043E\u0448\u043B\u0438!");
 		welcomeLabel.setForeground(new Color(0, 0, 128));
 		welcomeLabel.setBackground(SystemColor.activeCaption);
 		welcomeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		welcomeLabel.setFont(new Font("Calibri", Font.BOLD, 50));
-		GridBagConstraints gbc_welcomeLabel = new GridBagConstraints();
-		gbc_welcomeLabel.anchor = GridBagConstraints.NORTHWEST;
-		gbc_welcomeLabel.insets = new Insets(20, 0, 20, 0);
-		gbc_welcomeLabel.gridx = 1;
-		gbc_welcomeLabel.gridy = 0;
-		contentPane.add(welcomeLabel, gbc_welcomeLabel);
+		contentPane.add(welcomeLabel);
 		
-		JButton findLocationsButton = new JButton("\u041D\u0430\u043C\u0435\u0440\u0438 \u043B\u043E\u043A\u0430\u0446\u0438\u0438");
-		findLocationsButton.addMouseListener(new MouseAdapter() {
+		JButton categorizeCommentsButton = new JButton("Категоризирай коментари");
+		categorizeCommentsButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mousePressed(MouseEvent arg0) {
-				BayesAlgorithm.startTraining();
-				openLocationsTableView();
+			public void mousePressed(MouseEvent arg0) {				
+				openLocationsTableView(TableViewType.TableViewTypeCategorizeComments);
 			}
 		});
-		findLocationsButton.setForeground(new Color(230, 230, 250));
-		findLocationsButton.setFont(new Font("Calibri", Font.BOLD, 18));
-		findLocationsButton.setBackground(new Color(0, 0, 128));
-		findLocationsButton.setVerticalAlignment(SwingConstants.BOTTOM);
-		GridBagConstraints gbc_findLocationsButton = new GridBagConstraints();
-		gbc_findLocationsButton.anchor = GridBagConstraints.NORTH;
-		gbc_findLocationsButton.gridx = 1;
-		gbc_findLocationsButton.gridy = 1;
-		contentPane.add(findLocationsButton, gbc_findLocationsButton);
+		categorizeCommentsButton.setForeground(new Color(230, 230, 250));
+		categorizeCommentsButton.setFont(new Font("Calibri", Font.BOLD, 18));
+		categorizeCommentsButton.setBackground(Color.LIGHT_GRAY);
+		categorizeCommentsButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		contentPane.add(categorizeCommentsButton);
+		
+		JButton findHotelsButton = new JButton("Намери хотели");
+		findHotelsButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {				
+				openLocationsTableView(TableViewType.TableViewTypeFindHotels);
+			}
+		});
+		findHotelsButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		findHotelsButton.setForeground(new Color(230, 230, 250));
+		findHotelsButton.setFont(new Font("Calibri", Font.BOLD, 18));
+		findHotelsButton.setBackground(Color.LIGHT_GRAY);
+		contentPane.add(findHotelsButton);
 	}
 	
-	private void openLocationsTableView() {
-		LocationsTableView tableViewWindow = new LocationsTableView();
+	private void openLocationsTableView(TableViewType type) {
+		if(type == TableViewType.TableViewTypeCategorizeComments) {
+			BayesAlgorithm.startTraining();
+		} else if (type == TableViewType.TableViewTypeFindHotels) {
+			
+		}
+		LocationsTableView tableViewWindow = new LocationsTableView(type);
 		tableViewWindow.setDefaultCloseOperation(LocationsTableView.EXIT_ON_CLOSE);
 		tableViewWindow.setVisible(true);
 	}
